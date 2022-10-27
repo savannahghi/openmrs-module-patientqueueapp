90e087ef5e4fd5d51684d812914299d0256ffe21
VIC <ngarivictor43@gmail.com>
2022-10-28 00:27:24 +0300

    ci workflow

--- api/src/main/java/org/openmrs/module/patientqueueapp/PatientQueueConstants.java
+++ api/src/main/java/org/openmrs/module/patientqueueapp/PatientQueueConstants.java
@@ -5,7 +5,7 @@ public class PatientQueueConstants {
     public static final String MODULE_ID_1 ="patientdashboardapp" ;
     public static final String APP_DELIVERY_ROOM = MODULE_ID+".delivery";
     public static final String APP_MATERNITY_TRIAGE = MODULE_ID+".maternitytriage";
-    public static final String APP_MCH_CLINIC = MODULE_ID+".mchclinic";
+    public static final String APP_ONCOLOGY_CLINIC = MODULE_ID+".mchclinic";
     public static final String APP_MCH_TRIAGE = MODULE_ID+".mchtriage";
     public static final String APP_OPD = MODULE_ID+".opdqueue";
     public static final String APP_PAC = MODULE_ID+".pac";
